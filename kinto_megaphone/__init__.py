ea4499b0cba95118cd95bd3fe1833d9e8a3da680
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-09-20 19:13:22 -0400

    flake8

--- kinto_megaphone/__init__.py
+++ kinto_megaphone/__init__.py
@@ -6,6 +6,7 @@ __version__ = pkg_resources.get_distribution(__package__).version
 from .heartbeat import find_megaphone_url, MegaphoneHeartbeat
 from .megaphone import Megaphone
 
+
 def includeme(config):
     anon_client = Megaphone(find_megaphone_url(config.registry), None)
 
