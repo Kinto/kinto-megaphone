1b3d113de2eca7c0dc7a329b5a3860f71bd76dca
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-09-24 19:57:12 -0400

    Move module-config code into __init__
    This lets us re-use some of the sanity checks on the configuration.

--- kinto_megaphone/heartbeat.py
+++ kinto_megaphone/heartbeat.py
@@ -1,7 +1,3 @@
-from pyramid.config import ConfigurationError
-from pyramid.settings import aslist
-
-
 class MegaphoneHeartbeat(object):
     def __init__(self, client):
         self.client = client
@@ -9,17 +5,3 @@ class MegaphoneHeartbeat(object):
     def __call__(self, _request):
         data = self.client.heartbeat()
         return data['database'] == 'ok' and data['status'] == 'ok'
-
-
-def find_megaphone_url(registry):
-    # FIXME: this assumes only one Megaphone server per config
-    listeners = aslist(registry.settings['event_listeners'])
-    for listener in listeners:
-        prefix = 'event_listeners.{}'.format(listener)
-        listener_modpath = registry.settings['{}.use'.format(prefix)]
-        # FIXME: maybe be smarter about the current module's path?
-        if listener_modpath.startswith('kinto_megaphone.'):
-            return registry.settings['{}.url'.format(prefix)]
-
-    raise ConfigurationError(
-        "No megaphone listeners configured. Listeners are {}".format(listeners))
--- tests/test_heartbeat.py
+++ tests/test_heartbeat.py
@@ -35,30 +35,3 @@ def test_heartbeat_understands_failure(megaphone):
     }
 
     assert h(None) is False
-
-
-def test_find_megaphone_url_reads_settings():
-    registry = mock.Mock()
-    registry.settings = {
-        'event_listeners': 'mp',
-        'event_listeners.mp.use': 'kinto_megaphone.listeners',
-        'event_listeners.mp.url': 'http://example.com/',
-    }
-
-    assert heartbeat.find_megaphone_url(registry) == 'http://example.com/'
-
-
-def test_find_megaphone_url_raises_if_no_listeners_match():
-    registry = mock.Mock()
-    registry.settings = {
-        'event_listeners': 'mp megaphone meeegggaaaaphoooonnnee',
-        'event_listeners.mp.use': 'kinto_pusher.listeners',
-        'event_listeners.mp.url': 'http://example.com/',
-        'event_listeners.megaphone.use': 'kinto_fxa.listeners',
-        'event_listeners.megaphone.url': 'http://example.com/',
-        'event_listeners.meeegggaaaaphoooonnnee.use': 'kinto_portier.listeners',
-        'event_listeners.meeegggaaaaphoooonnnee.url': 'http://example.com/',
-    }
-
-    with pytest.raises(ConfigurationError):
-        heartbeat.find_megaphone_url(registry)
